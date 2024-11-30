3a8cda337cad3bf5abebbb0f5ff81eebdea45f71
mek-eva <mekaba2024@gmail.com>
2024-11-30 10:20:43 -0500

    Auth Css edit

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,5 +1,5 @@
 import React,{useState, useContext} from 'react'
-import classes from './signup.module.css'
+import classes from './Signup.module.css'
 import {Link, useNavigate, useLocation} from "react-router-dom"
 import {auth} from '../../Utility/FireBase';
 import {ClipLoader} from 'react-spinners';
